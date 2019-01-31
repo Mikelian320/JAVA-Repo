9fc552164637ad9faeb9cd1c2b2df167e8ec7842
Mikelian <lianxuejun@hust.edu.cn>
2019-01-31 10:43:39 +0800

    first commit

---
+++ Demo/Puppy.java
@@ -0,0 +1,18 @@
+public class Puppy{
+    int puppyAge;
+    public Puppy(String name){
+        System.out.println("The dog name is "+ name);
+    }
+    public void setAge(int age) {
+        puppyAge = age;
+    }
+    public int getAge() {
+        System.out.println("The dog age is "+ puppyAge);
+        return puppyAge;        
+    }
+    public static void main(String[] args) {
+        Puppy myPuppy= new Puppy("tommy");
+        myPuppy.setAge(2);
+        myPuppy.getAge();
+    }
+ }
